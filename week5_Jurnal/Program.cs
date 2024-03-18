7a785b708a2b92c1be970de4967525a618dacec5
unknown <kusumarizky65@gmail.com>
2024-03-18 07:23:47 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- week5_Jurnal/Program.cs
+++ week5_Jurnal/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class Penjumlahan<T>
+{
+    
+    //method
+    public void doSum<T>(List<T> input)
+    {
+        dynamic sum = 0.0;
+        for (int i = 0; i < input.Count; i++)
+        {
+            sum = sum + input[i];
+        }
+        Console.WriteLine("hasil sum = " + sum);
+    }
+
+   
+}
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Penjumlahan<double> testing = new Penjumlahan<double>();
+        List<double> input = new List<double>();
+        input.Add(13.0);
+        input.Add(2.0);
+        input.Add(22.0);
+        testing.doSum(input);
+    }
+}
